e741e8d6a8551e06ac5305294e55b1fb707748c5
balrom1981 <balrom@bk.ru>
2021-03-05 17:04:07 +0300

    project changed

--- src/test/java/ru/netology/web/CallBackTest.java
+++ src/test/java/ru/netology/web/CallBackTest.java
@@ -1,41 +1,14 @@
 package ru.netology.web;
 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import javax.swing.*;
-import java.awt.*;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CallBackTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    public static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void setupTest() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
     @Test
     void shouldSubmitRequest() {
@@ -74,6 +47,6 @@ class CallBackTest {
         $("[data-test-id=phone] input").setValue("+79270000000");
 //        $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $("[.input_invalid]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[.input_invalid").shouldHave(exactText("color: #ff5c5c!important"));
     }
 }
