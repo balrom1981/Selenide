f33c410baddf5dde2e37474220b11ab8108f6af0
balrom1981 <balrom@bk.ru>
2021-03-05 16:03:29 +0300

    project changed

--- src/test/java/ru/netology/web/CallBackTest.java
+++ src/test/java/ru/netology/web/CallBackTest.java
@@ -3,36 +3,15 @@ package ru.netology.web;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CallBackTest {
-    private WebDriver driver;
 
-    @BeforeAll
-    public static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void setupTest() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
     @Test
     void shouldSubmitRequest() {
