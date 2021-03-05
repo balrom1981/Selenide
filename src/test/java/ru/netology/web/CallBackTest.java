7b904696aba7a66ce96261aee07c70db99fb7385
balrom1981 <balrom@bk.ru>
2021-03-05 15:48:45 +0300

    Initial commit

--- src/test/java/ru/netology/web/CallBackTest.java
+++ src/test/java/ru/netology/web/CallBackTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import javax.swing.*;
+import java.awt.*;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -42,5 +45,35 @@ class CallBackTest {
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldRejectRequestByName() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Ivan");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[class=input__sub]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldRejectRequestByPhone() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+792700000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone] [class=input__sub]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldRejectRequestByCheckbox() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+//        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[.input_invalid]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 }
